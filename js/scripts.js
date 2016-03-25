f5ef9a4a4b4b5b601cc6e4214b46772cc6fd7cf9
Darius Capizzi <DariusCapizzi@gmail.com>
2016-03-25 10:06:25 -0700

    apply bootstrap css, and custom css

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ $(function(){
 
   $("form").submit(function(event){
     event.preventDefault();
+    $(".location_images div").hide();
 
     //variable declaration
     var Answer1 = $("input:radio[name=theQuestion]:checked").val();
@@ -25,19 +26,23 @@ $(function(){
     //logic
     if (Answer1 === "not to"){
       //should they choose not to live
-      $("h1").text("death")
+      $(".location_images h1").text("death");
+      $(".grave_image").show();
     }
     else if ( (Answer2 === "love") || (Answer3 === "kinda") ){
       // hamburgers and superman are on land
-      $("h1").text("how about on land?")
+      $(".location_images h1").text("how about... land?")
+      $(".land_image").show();
     }
     else if ( (Answer4 ==="why not?") || (Answer5 === "hellyes")) {
       // seafood and aquaman are under the sea
-      $("h1").text("how about under the sea?")
+      $(".location_images h1").text("how about... under the sea?")
+      $(".sea_image").show();
     }
     else {
-      $("h1").text("Why live if you don't like anything?");
+      $(".location_images h1").text("Why live if you don't like anything?");
       $("#die").attr('checked', true);
+      $(".grave_image").show();
     }
 
 
